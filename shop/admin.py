9f3e9669ec34428ee5dcb74bc70e1a1a2cb9705f
Delta Student <156696676+dineshade@users.noreply.github.com>
2024-02-15 17:50:37 +0530

    newmsg

---
+++ shop/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Products, Contact, Oraders, OrderUpdate
+
+admin.site.register(Products)
+admin.site.register(Contact)
+admin.site.register(Oraders)
+admin.site.register(OrderUpdate)
